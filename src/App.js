859d277bb40521dcec7448d2603d274ce0d66c94
Lee Motley <lee_motley@hotmail.com>
2018-08-15 22:35:03 +0100

    Baseline react app, homepage and initial palette

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="main-content">
+          <h1>No taste like home...</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
